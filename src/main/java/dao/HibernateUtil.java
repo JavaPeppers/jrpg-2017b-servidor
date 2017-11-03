47d7c1242cd10a6fe8529279fcc8bfb759d9e290
FGPena <facundogarciapena@hotmail.com>
2017-11-03 00:42:48 -0300

    codigo horrible, basicamente pruebas. se agregan closeThreadSession y openThreadSession

--- src/main/java/dao/HibernateUtil.java
+++ src/main/java/dao/HibernateUtil.java
@@ -1,18 +1,23 @@
 package dao;
 
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
 
 	static {
 
 		try {
-
 			sessionFactory = new Configuration().configure()
 			.buildSessionFactory();
 
@@ -21,14 +26,20 @@ public class HibernateUtil {
 			System.err.println("Initial SessionFactory creation failed." + ex);
 
 			throw new ExceptionInInitializerError(ex);
-
 		}
 
 	}
-
 	public static SessionFactory getSessionFactory() {
-
 		return sessionFactory;
-
 	}
+	public static void openThreadSession() {
+   	 Session session = sessionFactory.openSession();
+   	 ThreadLocalSessionContext.bind(session);
+    }
+	
+	 public static void closeThreadSession() {
+	        Session session = ThreadLocalSessionContext.unbind(sessionFactory);
+	        if (session!=null)
+	            session.close();
+	 }
 }
